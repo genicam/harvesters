445e74a5e8d61fa6b1b34370d6a46353da136fdc
kznr <who.is.kazunari@gmail.com>
2018-06-13 00:46:25 +0900

    Prepare a shell script that creates & uploads the distribution package

---
+++ upload.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+#
+# Copyright 2018 EMVA
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ----------------------------------------------------------------------------
+
+# Prepare options.
+test=
+upload=
+
+while getopts tu opt ; do
+  case $opt in
+  t)
+    test=true ;;
+  u)
+    upload=true ;;
+  \?)
+    echo "Invalid option!"
+    exit 1 ;;
+  esac
+done
+
+# Delete intermediate directories.
+for dir in build dist genicam.harvester.egg-info
+do
+  if [ -e "$dir" ]
+  then
+    echo "Removing \"$dir\""
+    rm -rf "$dir"
+  fi
+done
+
+# Create a distribution package.
+python3 setup.py sdist bdist_wheel
+
+url="https://upload.pypi.org/legacy/"
+if [ "x$test" = "xtrue" ]
+then
+  url="https://test.pypi.org/legacy/"
+fi
+
+# Upload the distribution package.
+if [ "x$upload" = "xtrue" ]
+then
+  twine upload --repository-url $url dist/*
+fi
+
+exit $?
+

864af946dd87f7d5a7a28fdcc9227a584c6bdc94
kznr <who.is.kazunari@gmail.com>
2018-07-31 20:46:26 +0900

    Set target directories

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-#import os
-#import sys
+import os
+import sys
 #sys.path.insert(0, os.path.abspath('@CMAKE_RUNTIME_OUTPUT_DIRECTORY@'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.dirname(__file__))
 
 # -- General configuration ------------------------------------------------
 
@@ -142,7 +144,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = '@SPHINX_THEME@'
+html_theme = 'bizstyle'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme

4227b1057427de84633a6bd9b65cbe2a7e30727a
kznr <who.is.kazunari@gmail.com>
2018-07-31 22:15:28 +0900

    Try documenting API

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,7 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('@CMAKE_RUNTIME_OUTPUT_DIRECTORY@'))
-sys.path.insert(0, os.path.abspath('../harvesters'))
-sys.path.append(os.path.dirname(__file__))
+sys.path.insert(0, os.path.abspath('../harvesters/'))
 
 # -- General configuration ------------------------------------------------
 

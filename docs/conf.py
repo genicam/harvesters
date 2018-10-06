5b11e384c96acd678c25f1bcde99cd9149f0537b
kznr <who.is.kazunari@gmail.com>
2018-10-06 11:27:30 +0900

    Update the package directory

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/harvesters/'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 # -- General configuration ------------------------------------------------
 

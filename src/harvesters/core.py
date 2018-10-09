e326309dbb041d384146ab742f3413b5b1530507
kznr <who.is.kazunari@gmail.com>
2018-10-09 13:11:45 +0900

    Resolve issue #47
    We had to detach a buffer from the chunk adapter and discard the adapter
    before destructing a ImageAcquirer object.

--- src/harvesters/core.py
+++ src/harvesters/core.py
@@ -2177,6 +2177,16 @@ class Harvester:
 
             #
             if ia.device.node_map:
+                #
+                if ia._chunk_adapter:
+                    ia._chunk_adapter.detach_buffer()
+                    ia._chunk_adapter = None
+                    self._logger.info(
+                        'Detached a buffer from the chunk adapter of {0}.'.format(
+                            id_
+                        )
+                    )
+
                 ia.device.node_map.disconnect()
                 self._logger.info(
                     'Disconnected the port from the NodeMap of {0}.'.format(

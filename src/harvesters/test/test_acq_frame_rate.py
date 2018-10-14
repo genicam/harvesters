7baaa80b708ddfbde6468835b076c0e0e3d47672
kznr <who.is.kazunari@gmail.com>
2018-10-14 23:06:05 +0900

    Record image acquisition statistics

--- src/harvesters/test/test_acq_frame_rate.py
+++ src/harvesters/test/test_acq_frame_rate.py
@@ -19,6 +19,7 @@
 
 
 # Standard library imports
+import datetime
 from threading import Thread
 import time
 import unittest
@@ -29,7 +30,7 @@ import unittest
 from harvesters.test.base_harvester import TestHarvesterCoreBase
 
 
-class ThreadImageAcquisition(Thread):
+class ThreadImageAcquisitionStatistics(Thread):
     def __init__(self, *, worker=None, timeout_s=0):
         #
         super().__init__()
@@ -47,7 +48,7 @@ class ThreadImageAcquisition(Thread):
             #
             if self._worker:
                 self._worker()
-                time.sleep(0)
+                time.sleep(1)
 
             #
             diff_s = time.time() - self._time_base
@@ -65,8 +66,8 @@ class TestTutorials(TestHarvesterCoreBase):
         self.ia.start_image_acquisition()
 
         # Run the image acquisition thread:
-        thread = ThreadImageAcquisition(
-            worker=self._worker_acq_images, timeout_s=3
+        thread = ThreadImageAcquisitionStatistics(
+            worker=self._worker_update_statistics, timeout_s=10
         )
         thread.start()
         thread.join()
@@ -74,9 +75,27 @@ class TestTutorials(TestHarvesterCoreBase):
         # Stop image acquisition:
         self.ia.stop_image_acquisition()
 
-    def _worker_acq_images(self):
-        # Just let the image acquirer acquires images:
-        pass
+    def _worker_update_statistics(self):
+        #
+        if self.ia:
+            message_config = 'W: {0} x H: {1}, {2}, '.format(
+                self.ia.device.node_map.Width.value,
+                self.ia.device.node_map.Height.value,
+                self.ia.device.node_map.PixelFormat.value
+            )
+
+            message_statistics = '{0:.1f} fps, elapsed {1}, {2} images'.format(
+                self.ia.statistics.fps,
+                str(datetime.timedelta(
+                    seconds=int(self.ia.statistics.elapsed_time_s)
+                )),
+                self.ia.statistics.num_images
+            )
+
+            #
+            self._logger.info(
+                '{0}'.format(message_config + message_statistics)
+            )
 
 
 if __name__ == '__main__':

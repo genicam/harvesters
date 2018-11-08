4ab870e252ad8a4cbea36dcfb2cb8a1f8181cf10
kznr <who.is.kazunari@gmail.com>
2018-11-08 12:38:13 +0900

    Update the PFNC list

--- src/harvesters/util/pfnc.py
+++ src/harvesters/util/pfnc.py
@@ -89,122 +89,322 @@ def get_bits_per_pixel(data_format):
 
 mono_location_formats = [
     #
-    'Raw', 'Mono', 'R', 'G', 'B',
+    'Mono8',
+    'Mono8s',
+    'Mono10',
+    'Mono12',
+    'Mono14',
+    'Mono16',
     #
-    'Mono8', 'Mono10', 'Mono12', 'Mono14', 'Mono16',
+    'R8',
+    'R10',
+    'R12',
+    'R16',
+    'G8',
+    'G10',
+    'G12',
+    'G16',
+    'B8',
+    'B10',
+    'B12',
+    'B16',
     #
-    'Coord3D_A', 'Coord3D_B', 'Coord3D_C',
-    'Coord3D_A8', 'Coord3D_B8', 'Coord3D_C8',
-    'Coord3D_A16', 'Coord3D_B16', 'Coord3D_C16',
-    'Coord3D_A32f', 'Coord3D_B32f', 'Coord3D_C32f',
-    'Coord3D_A10p', 'Coord3D_B10p', 'Coord3D_C10p',
-    'Coord3D_A12p', 'Coord3D_B12p', 'Coord3D_C12p',
+    'Coord3D_A8',
+    'Coord3D_B8',
+    'Coord3D_C8',
+    'Coord3D_A16',
+    'Coord3D_B16',
+    'Coord3D_C16',
+    'Coord3D_A32f',
+    'Coord3D_B32f',
+    'Coord3D_C32f',
     #
     'Confidence1',
-    'Confidence8', 'Confidence16', 'Confidence32f',
+    'Confidence8',
+    'Confidence16',
+    'Confidence32f',
+]
 
+mono_packed_location_formats = [
+    'Mono1p',
+    'Mono2p',
+    'Mono4p',
+    'Mono10Packed',
+    'Mono10p',
+    'Mono12Packed',
+    'Mono12p',
+    'Coord3D_A10p',
+    'Coord3D_B10p',
+    'Coord3D_C10p',
+    'Coord3D_A12p',
+    'Coord3D_B12p',
+    'Coord3D_C12p',
 ]
 
 lmn_444_location_formats = [
-    'RGB', 'BGR',
-    'YUV', 'YCbCr', 'YCbCr601', 'YCbCr709',
-    'RGB8', 'RGB10', 'RGB12', 'RGB14', 'RGB16',
-    'BGR8', 'BGR10', 'BGR12', 'BGR14', 'BGR16',
+    #
+    'RGB8',
+    'RGB10',
+    'RGB12',
+    'RGB14',
+    'RGB16',
+    #
+    'BGR8',
+    'BGR10',
+    'BGR12',
+    'BGR14',
+    'BGR16',
+    #
+    'Coord3D_ABC8',
+    'Coord3D_ABC8_Planar',
+    'Coord3D_ABC16',
+    'Coord3D_ABC16_Planar',
+    'Coord3D_ABC32f',
+    'Coord3D_ABC32f_Planar',
+]
+
+lmn_444_packed_location_formats = [
+    #
     'RGB8Packed',
-    'Coord3D_ABC8', 'Coord3D_ABC8_Planar',
-    'Coord3D_ABC16', 'Coord3D_ABC16_Planar',
-    'Coord3D_ABC32f', 'Coord3D_ABC32f_Planar',
-    'Coord3D_ABC10p', 'Coord3D_ABC10p_Planar',
-    'Coord3D_ABC12p', 'Coord3D_ABC12p_Planar',
+    #
+    'Coord3D_ABC10p',
+    'Coord3D_ABC10p_Planar',
+    'Coord3D_ABC12p',
+    'Coord3D_ABC12p_Planar',
 ]
 
 lmn_422_location_formats = [
-    'YUV422', 'YCbCr422', 'YCbCr601_422', 'YCbCr709_422',
+    'YUV422_8_UYVY',
+    'YUV422_8',
+    'YCbCr422_8',
+    'YCbCr601_422_8',
+    'YCbCr709_422_8',
+    'YCbCr422_8_CbYCrY',
+    'YCbCr601_422_8_CbYCrY',
+    'YCbCr709_422_8_CbYCrY',
+    'YCbCr422_10',
+    'YCbCr422_12',
+    'YCbCr601_422_10',
+    'YCbCr601_422_12',
+    'YCbCr709_422_10',
+    'YCbCr709_422_12',
+    'YCbCr422_10_CbYCrY',
+    'YCbCr422_12_CbYCrY',
+    'YCbCr601_422_10_CbYCrY',
+    'YCbCr601_422_12_CbYCrY',
+    'YCbCr709_422_10_CbYCrY',
+    'YCbCr709_422_12_CbYCrY',
+    'YCbCr2020_422_8',
+    'YCbCr2020_422_8_CbYCrY',
+    'YCbCr2020_422_10',
+    'YCbCr2020_422_10_CbYCrY',
+    'YCbCr2020_422_12',
+    'YCbCr2020_422_12_CbYCrY',
+]
+
+lmn_422_packed_location_formats = [
+    'YCbCr422_10p',
+    'YCbCr422_12p',
+    'YCbCr601_422_10p',
+    'YCbCr601_422_12p',
+    'YCbCr709_422_10p',
+    'YCbCr709_422_12p',
+    'YCbCr422_10p_CbYCrY',
+    'YCbCr422_12p_CbYCrY',
+    'YCbCr601_422_10p_CbYCrY',
+    'YCbCr601_422_12p_CbYCrY',
+    'YCbCr709_422_10p_CbYCrY',
+    'YCbCr709_422_12p_CbYCrY',
+    'YCbCr2020_422_10p',
+    'YCbCr2020_422_10p_CbYCrY',
+    'YCbCr2020_422_12p',
+    'YCbCr2020_422_12p_CbYCrY',
 ]
 
 lmn_411_location_formats = [
-    'YUV411', 'YCbCr411', 'YCbCr601_411', 'YCbCr709_411',
+    'YUV411_8_UYYVYY',
+    'YCbCr411_8_CbYYCrYY',
+    'YCbCr601_411_8_CbYYCrYY',
+    'YCbCr709_411_8_CbYYCrYY',
+    'YCbCr411_8',
+    'YCbCr2020_411_8_CbYYCrYY',
 ]
 
 lmno_4444_location_formats = [
-    'aRGB', 'YRGB', 'RGBa', 'BGRa',
-    'RGBa8', 'RGBa10', 'RGBa12', 'RGBa14', 'RGBa16',
-    'BGRa8', 'BGRa10', 'BGRa12', 'BGRa14', 'BGRa16',
+    'RGBa8',
+    'RGBa10',
+    'RGBa12',
+    'RGBa14',
+    'RGBa16',
+    'BGRa8',
+    'BGRa10',
+    'BGRa12',
+    'BGRa14',
+    'BGRa16',
+]
+
+lmno_4444_packed_location_formats = [
+    'RGBa10p',
+    'RGBa12p',
+    'BGRa10p',
+    'BGRa12p',
 ]
 
 lm_44_location_formats = [
-    'Coord3D_AC', 'Coord3D_AC8', 'Coord3D_AC8_Planar',
-    'Coord3D_AC16', 'Coord3D_AC16_Planar',
-    'Coord3D_AC32f', 'Coord3D_AC32f_Planar',
-    'Coord3D_AC10p', 'Coord3D_AC10p_Planar',
-    'Coord3D_AC12p', 'Coord3D_AC12p_Planar',
+    'Coord3D_AC8',
+    'Coord3D_AC8_Planar',
+    'Coord3D_AC16',
+    'Coord3D_AC16_Planar',
+    'Coord3D_AC32f',
+    'Coord3D_AC32f_Planar',
+]
+
+lm_44_packed_location_formats = [
+    'Coord3D_AC10p',
+    'Coord3D_AC10p_Planar',
+    'Coord3D_AC12p',
+    'Coord3D_AC12p_Planar',
 ]
 
 bayer_location_formats = [
-    #
-    'BayerGR8', 'BayerGB8', 'BayerRG8', 'BayerBG8',
-    #
-    'BayerGR12', 'BayerGB12', 'BayerRG12', 'BayerBG12',
-    #
-    'BayerGR10', 'BayerGB10', 'BayerRG10', 'BayerBG10',
-    #
-    'BayerGR16', 'BayerRG16', 'BayerGB16', 'BayerBG16',
-    #
-    'BayerGR32', 'BayerRG32', 'BayerGB32', 'BayerBG32',
+    'BayerGR8',
+    'BayerRG8',
+    'BayerGB8',
+    'BayerBG8',
+    'BayerGR10',
+    'BayerRG10',
+    'BayerGB10',
+    'BayerBG10',
+    'BayerGR12',
+    'BayerRG12',
+    'BayerGB12',
+    'BayerBG12',
+    'BayerGR16',
+    'BayerRG16',
+    'BayerGB16',
+    'BayerBG16',
+]
+
+bayer_packed_location_formats = [
+    'BayerGR10Packed',
+    'BayerRG10Packed',
+    'BayerGB10Packed',
+    'BayerBG10Packed',
+    'BayerGR12Packed',
+    'BayerRG12Packed',
+    'BayerGB12Packed',
+    'BayerBG12Packed',
+    'BayerBG10p',
+    'BayerBG12p',
+    'BayerGB10p',
+    'BayerGB12p',
+    'BayerGR10p',
+    'BayerGR12p',
+    'BayerRG10p',
+    'BayerRG12p',
 ]
 
 uint8_formats = [
     #
     'Mono8',
     #
-    'RGB8', 'RGB8Packed', 'RGBa8',
+    'RGB8',
+    'RGB8Packed',
+    'RGBa8',
     #
-    'BGR8', 'BGRa8',
+    'BGR8',
+    'BGRa8',
     #
-    'BayerGR8', 'BayerGB8', 'BayerRG8', 'BayerBG8',
+    'BayerGR8',
+    'BayerGB8',
+    'BayerRG8',
+    'BayerBG8',
     #
-    'Coord3D_A8', 'Coord3D_B8', 'Coord3D_C8',
-    'Coord3D_ABC8', 'Coord3D_ABC8_Planar',
-    'Coord3D_AC8', 'Coord3D_AC8_Planar',
+    'Coord3D_A8',
+    'Coord3D_B8',
+    'Coord3D_C8',
+    'Coord3D_ABC8',
+    'Coord3D_ABC8_Planar',
+    'Coord3D_AC8',
+    'Coord3D_AC8_Planar',
     #
+    'Confidence1',
     'Confidence8',
 ]
 
 uint16_formats = [
     #
-    'Mono10', 'Mono12', 'Mono14', 'Mono16',
+    'Mono10',
+    'Mono12',
+    'Mono14',
+    'Mono16',
     #
-    'RGB10', 'RGB12', 'RGB14', 'RGB16',
+    'RGB10',
+    'RGB12',
+    'RGB14',
+    'RGB16',
     #
-    'BGR10', 'BGR12', 'BGR14', 'BGR16',
+    'BGR10',
+    'BGR12',
+    'BGR14',
+    'BGR16',
     #
-    'RGBa10', 'RGBa12', 'RGBa14', 'RGBa16',
+    'RGBa10',
+    'RGBa12',
+    'RGBa14',
+    'RGBa16',
     #
-    'BGRa10', 'BGRa12', 'BGRa14', 'BGRa16',
+    'BGRa10',
+    'BGRa12',
+    'BGRa14',
+    'BGRa16',
     #
-    'BayerGR10', 'BayerGB10', 'BayerRG10', 'BayerBG10',
+    'BayerGR10',
+    'BayerGB10',
+    'BayerRG10',
+    'BayerBG10',
     #
-    'BayerGR12', 'BayerGB12', 'BayerRG12', 'BayerBG12',
+    'BayerGR12',
+    'BayerGB12',
+    'BayerRG12',
+    'BayerBG12',
     #
-    'BayerGR16', 'BayerRG16', 'BayerGB16', 'BayerBG16',
+    'BayerGR16',
+    'BayerRG16',
+    'BayerGB16',
+    'BayerBG16',
     #
-    'Coord3D_A16', 'Coord3D_B16', 'Coord3D_C16',
+    'Coord3D_A16',
+    'Coord3D_B16',
+    'Coord3D_C16',
     #
-    'Coord3D_ABC16', 'Coord3D_ABC16_Planar',
+    'Coord3D_ABC16',
+    'Coord3D_ABC16_Planar',
     #
-    'Coord3D_AC16', 'Coord3D_AC16_Planar',
+    'Coord3D_AC16',
+    'Coord3D_AC16_Planar',
     #
-    'Coord3D_A10p', 'Coord3D_B10p', 'Coord3D_C10p',
+    'Coord3D_A10p',
+    'Coord3D_B10p',
+    'Coord3D_C10p',
     #
-    'Coord3D_A12p', 'Coord3D_B12p', 'Coord3D_C12p',
+    'Coord3D_A12p',
+    'Coord3D_B12p',
+    'Coord3D_C12p',
     #
-    'Coord3D_ABC10p', 'Coord3D_ABC10p_Planar',
+    'Coord3D_ABC10p',
+    'Coord3D_ABC10p_Planar',
     #
-    'Coord3D_ABC12p', 'Coord3D_ABC12p_Planar',
+    'Coord3D_ABC12p',
+    'Coord3D_ABC12p_Planar',
     #
-    'Coord3D_AC10p', 'Coord3D_AC10p_Planar',
+    'Coord3D_AC10p',
+    'Coord3D_AC10p_Planar',
     #
-    'Coord3D_AC12p', 'Coord3D_AC12p_Planar',
+    'Coord3D_AC12p',
+    'Coord3D_AC12p_Planar',
+    #
+    'Confidence16',
 ]
 
 uint32_formats = [
@@ -213,22 +413,33 @@ uint32_formats = [
 
 float32_formats = [
     #
-    'Coord3D_A32f', 'Coord3D_B32f', 'Coord3D_C32f',
+    'Coord3D_A32f',
+    'Coord3D_B32f',
+    'Coord3D_C32f',
     #
-    'Coord3D_ABC32f', 'Coord3D_ABC32f_Planar',
+    'Coord3D_ABC32f',
+    'Coord3D_ABC32f_Planar',
     #
-    'Coord3D_AC32f', 'Coord3D_AC32f_Planar',
+    'Coord3D_AC32f',
+    'Coord3D_AC32f_Planar',
+    #
+    'Confidence32f',
 ]
 
 component_8bit_formats = [
     #
     'Mono8',
     #
-    'RGB8', 'RGBa8',
+    'RGB8',
+    'RGBa8',
     #
-    'BGR8', 'BGRa8',
+    'BGR8',
+    'BGRa8',
     #
-    'BayerGR8', 'BayerGB8', 'BayerRG8', 'BayerBG8',
+    'BayerGR8',
+    'BayerGB8',
+    'BayerRG8',
+    'BayerBG8',
     #
     'Confidence8',
 ]
@@ -237,105 +448,201 @@ component_10bit_formats = [
     #
     'Mono10',
     #
-    'RGB10', 'RGBa10',
+    'RGB10',
+    'RGBa10',
     #
-    'BGR10', 'BGRa10',
+    'BGR10',
+    'BGRa10',
     #
-    'BayerGR10', 'BayerGB10', 'BayerRG10', 'BayerBG10',
+    'BayerGR10',
+    'BayerGB10',
+    'BayerRG10',
+    'BayerBG10',
 ]
 
 component_12bit_formats = [
     #
     'Mono12',
     #
-    'RGB12', 'RGBa12',
+    'RGB12',
+    'RGBa12',
     #
-    'BGR12', 'BGRa12',
+    'BGR12',
+    'BGRa12',
     #
-    'BayerGR12', 'BayerGB12', 'BayerRG12', 'BayerBG12',
+    'BayerGR12',
+    'BayerGB12',
+    'BayerRG12',
+    'BayerBG12',
 ]
 
 component_14bit_formats = [
     #
     'Mono14',
     #
-    'RGB14', 'RGBa14',
+    'RGB14',
+    'RGBa14',
     #
-    'BGR14', 'BGRa14',
+    'BGR14',
+    'BGRa14',
 ]
 
 component_16bit_formats = [
     #
     'Mono16',
     #
-    'RGB16', 'RGBa16',
+    'RGB16',
+    'RGBa16',
     #
-    'BayerGR16', 'BayerRG16', 'BayerGB16', 'BayerBG16',
+    'BayerGR16',
+    'BayerRG16',
+    'BayerGB16',
+    'BayerBG16',
     #
-    'Coord3D_A16', 'Coord3D_B16', 'Coord3D_C16',
+    'Coord3D_A16',
+    'Coord3D_B16',
+    'Coord3D_C16',
     #
-    'Coord3D_ABC16', 'Coord3D_ABC16_Planar',
+    'Coord3D_ABC16',
+    'Coord3D_ABC16_Planar',
     #
-    'Coord3D_AC16', 'Coord3D_AC16_Planar',
+    'Coord3D_AC16',
+    'Coord3D_AC16_Planar',
+    #
+    'Confidence16',
+]
+
+component_32bit_formats = [
+    'Confidence32f',
 ]
 
 component_2d_formats = [
     #
-    'Mono8', 'Mono10', 'Mono12', 'Mono14', 'Mono16',
+    'Mono8',
+    'Mono10',
+    'Mono12',
+    'Mono14',
+    'Mono16',
     #
-    'RGB8', 'RGB10', 'RGB12', 'RGB14', 'RGB16',
+    'RGB8',
+    'RGB10',
+    'RGB12',
+    'RGB14',
+    'RGB16',
     #
-    'BGR8', 'BGR10', 'BGR12', 'BGR14', 'BGR16',
+    'BGR8',
+    'BGR10',
+    'BGR12',
+    'BGR14',
+    'BGR16',
     #
-    'RGBa8', 'RGBa10', 'RGBa12', 'RGBa14', 'RGBa16',
+    'RGBa8',
+    'RGBa10',
+    'RGBa12',
+    'RGBa14',
+    'RGBa16',
     #
-    'BGRa8', 'BGRa10', 'BGRa12', 'BGRa14', 'BGRa16',
+    'BGRa8',
+    'BGRa10',
+    'BGRa12',
+    'BGRa14',
+    'BGRa16',
     #
-    'BayerGR8', 'BayerGB8', 'BayerRG8', 'BayerBG8',
+    'BayerGR8',
+    'BayerGB8',
+    'BayerRG8',
+    'BayerBG8',
     #
-    'BayerGR10', 'BayerGB10', 'BayerRG10', 'BayerBG10',
+    'BayerGR10',
+    'BayerGB10',
+    'BayerRG10',
+    'BayerBG10',
     #
-    'BayerGR12', 'BayerGB12', 'BayerRG12', 'BayerBG12',
+    'BayerGR12',
+    'BayerGB12',
+    'BayerRG12',
+    'BayerBG12',
     #
-    'BayerGR16', 'BayerRG16', 'BayerGB16', 'BayerBG16',
+    'BayerGR16',
+    'BayerRG16',
+    'BayerGB16',
+    'BayerBG16',
     #
-    'Coord3D_A8', 'Coord3D_B8', 'Coord3D_C8',
-    'Coord3D_ABC8', 'Coord3D_ABC8_Planar',
-    'Coord3D_AC8', 'Coord3D_AC8_Planar',
-    'Coord3D_A16', 'Coord3D_B16', 'Coord3D_C16',
-    'Coord3D_ABC16', 'Coord3D_ABC16_Planar',
-    'Coord3D_AC16', 'Coord3D_AC16_Planar',
-    'Coord3D_A32f', 'Coord3D_B32f', 'Coord3D_C32f',
-    'Coord3D_ABC32f', 'Coord3D_ABC32f_Planar',
-    'Coord3D_AC32f', 'Coord3D_AC32f_Planar',
-    'Coord3D_A10p', 'Coord3D_B10p', 'Coord3D_C10p',
-    'Coord3D_A12p', 'Coord3D_B12p', 'Coord3D_C12p',
-    'Coord3D_ABC10p', 'Coord3D_ABC10p_Planar',
-    'Coord3D_ABC12p', 'Coord3D_ABC12p_Planar',
-    'Coord3D_AC10p', 'Coord3D_AC10p_Planar',
-    'Coord3D_AC12p', 'Coord3D_AC12p_Planar',
-
+    'Coord3D_A8',
+    'Coord3D_B8',
+    'Coord3D_C8',
+    'Coord3D_ABC8',
+    'Coord3D_ABC8_Planar',
+    'Coord3D_AC8',
+    'Coord3D_AC8_Planar',
+    'Coord3D_A16',
+    'Coord3D_B16',
+    'Coord3D_C16',
+    'Coord3D_ABC16',
+    'Coord3D_ABC16_Planar',
+    'Coord3D_AC16',
+    'Coord3D_AC16_Planar',
+    'Coord3D_A32f',
+    'Coord3D_B32f',
+    'Coord3D_C32f',
+    'Coord3D_ABC32f',
+    'Coord3D_ABC32f_Planar',
+    'Coord3D_AC32f',
+    'Coord3D_AC32f_Planar',
+    'Coord3D_A10p',
+    'Coord3D_B10p',
+    'Coord3D_C10p',
+    'Coord3D_A12p',
+    'Coord3D_B12p',
+    'Coord3D_C12p',
+    'Coord3D_ABC10p',
+    'Coord3D_ABC10p_Planar',
+    'Coord3D_ABC12p',
+    'Coord3D_ABC12p_Planar',
+    'Coord3D_AC10p',
+    'Coord3D_AC10p_Planar',
+    'Coord3D_AC12p',
+    'Coord3D_AC12p_Planar',
     #
-    'Confidence1', 'Confidence1p', 'Confidence8', 'Confidence16',
+    'Confidence1',
+    'Confidence1p',
+    'Confidence8',
+    'Confidence16',
     'Confidence32f',
 ]
 
 rgb_formats = [
     #
-    'RGB8', 'RGB10', 'RGB12', 'RGB14', 'RGB16',
+    'RGB8',
+    'RGB10',
+    'RGB12',
+    'RGB14',
+    'RGB16',
 ]
 
 rgba_formats = [
     #
-    'RGBa8', 'RGBa10', 'RGBa12', 'RGBa14', 'RGBa16',
+    'RGBa8',
+    'RGBa10',
+    'RGBa12',
+    'RGBa14',
+    'RGBa16',
 ]
 
 bgr_formats = [
     #
-    'BGR8', 'BGR10', 'BGR12', 'BGR14', 'BGR16',
+    'BGR8',
+    'BGR10',
+    'BGR12',
+    'BGR14',
+    'BGR16',
 ]
 
 bgra_formats = [
     #
-    'BGRa8', 'BGRa10', 'BGRa12', 'BGRa14', 'BGRa16',
+    'BGRa8',
+    'BGRa10',
+    'BGRa12',
+    'BGRa14',
+    'BGRa16',
 ]

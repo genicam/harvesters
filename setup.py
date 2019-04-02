4430ff7ac84e1f2c7d6e88af83f802f8b41ae5b4
kznr <who.is.kazunari@gmail.com>
2019-04-02 14:46:26 +0900

    Revert "Shorten the description"
    This reverts commit 3962e65f65fb1cdd35aaea58839eea54b5622b82.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ if sys.version_info in supported_versions:
 with open('README.rst', 'r',encoding='utf-8_sig') as fh:
     __doc__ = fh.read()
 
-description = 'Image acquisition front-end library with Python'
+description = 'Image acquisition front-end library with Python (Maintained by volunteers of the official GenICam committee)'
 
 # Determine the base directory:
 base_dir = os.path.dirname(__file__)

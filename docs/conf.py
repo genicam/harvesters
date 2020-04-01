fdd5e425b1fb65d82b093adf2978b659ce17eedf
kznr <who.is.kazunari@gmail.com>
2020-04-01 22:12:10 +0900

    Update the theme to apply

--- docs/conf.py
+++ docs/conf.py
@@ -142,7 +142,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'bizstyle'
+html_theme = 'bizstyle'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
